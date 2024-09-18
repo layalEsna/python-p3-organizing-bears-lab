68fd7f75af90f03d4e392a5343d67aabe3e26ac9
Amene <layla_1380@yahoo.com>
2024-09-18 18:31:35 -0500

    bears table

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  sex TEXT,
+  color TEXT,
+  age INTEGER,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, sex, color, age, temperament, alive) 
+VALUES
+(1,'Mr. Chocolate', 'M', 'brown', 12, 'cool', 1),
+(2,'Rowdy', 'M', 'yellow', 21, 'ok', 0),
+(3,'Tabitha', 'M', 'brown', 5, 'cool', 1),
+(4,'Sergeant Brown', 'M', 'brown', 8, 'warm', 1),
+(5,'Melissa', 'F', 'black', 16, 'sensitive', 0),
+(6,'Grinch', 'M', 'brown', 12, 'cool', 1),
+(7,'Wendy', 'M', 'brown', 10, 'happy', 1),
+(8,NULL, 'F', 'white', 1, 'cold', 1);
